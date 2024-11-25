6d84e74969a76b21efb21c9861f0a156a89807aa
soorajas1210 <soorajasofficial@gmail.com>
2024-11-25 15:00:08 +0530

    project updates

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,7 +1,3 @@
-import React from "react";
-import mock01 from "../assets/images/mock01.png";
-import mock02 from "../assets/images/mock02.png";
-
 import "../assets/styles/Project.scss";
 import { projects } from "../data/projectData";
 
@@ -26,48 +22,6 @@ function Project() {
             <p>{project?.description}</p>
           </div>
         ))}
-
-        <div className="project">
-          <a
-            href="https://hookele.byuh.edu/transfer-evaluation-guidelines-and-matrix"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={mock02} className="zoom" alt="thumbnail" width="100%" />
-          </a>
-          <a
-            href="https://hookele.byuh.edu/transfer-evaluation-guidelines-and-matrix"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <h2>Transfer Evaluation Matrix</h2>
-          </a>
-          <p>
-            Created an interactive CSV table generator with Java, Handlebars,
-            and LESS. This project helps transfer students to quickly identify
-            eligible credits.
-          </p>
-        </div>
-        <div className="project">
-          <a
-            href="https://github.com/yujisatojr/submeowrine"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={mock01} className="zoom" alt="thumbnail" width="100%" />
-          </a>
-          <a
-            href="https://github.com/yujisatojr/submeowrine"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <h2>Submeowrine</h2>
-          </a>
-          <p>
-            Developed and released an Android mobile application using Java and
-            Android Studio that runs a 2D shooting game.
-          </p>
-        </div>
       </div>
     </div>
   );
--- src/data/projectData.js
+++ src/data/projectData.js
@@ -6,15 +6,15 @@ import project5 from "../assets/images/projects/M_hospitality.png";
 import project6 from "../assets/images/projects/Data_Management.png";
 import project7 from "../assets/images/projects/6783.jpg";
 import project8 from "../assets/images/projects/6783.jpg";
-import project9 from "../assets/images/projects/Data_Management.png";
-import project10 from "../assets/images/projects/Data_Management.png";
+import project9 from "../assets/images/projects/homey.png";
+import project10 from "../assets/images/projects/healthme.png";
 
 export const projects = [
   {
     img: project1,
     name: "Adventure sports Booking",
     description:
-      "Developed a dynamic booking application where users can explore and reserve a variety of adventure sports, such as diving, speedboat rides, and banana boat rides, with features to add items to a cart and complete bookings through secure payment.",
+      "Developed a dynamic booking application where users can explore and reserve a variety of adventure sports, such as diving, speedboat rides, and banana boat rides, with features to add items to a cart and complete bookings through secure payment. And also added SEO to the application",
     link: "https://www.adventuresports.ae",
   },
   {
@@ -27,40 +27,57 @@ export const projects = [
   {
     img: project3,
     name: "Aldhafra",
-    description: "",
+    description:
+      "Built a responsive website for Al Dhafra Property Management using Next.js and Strapi CMS, showcasing their real estate services with seamless navigation and efficient content management.",
     link: "https://www.aldhpm.com",
   },
   {
     img: project4,
     name: "BRS Website",
     description:
-      "Created a website for MBK Marine Industries, showcasing their expertise in shipbuilding, repair, and conversion services for UAE ports.",
+      "Developed a responsive website for Bright Riders School using Next.js and Strapi CMS, highlighting their CBSE curriculum from Kindergarten to Grade 12 and providing easy access to essential information for parents and students.",
     link: "https://brsnew-frontend.vercel.app",
   },
   {
     img: project5,
     name: "M Hospitality",
-    description: "",
+    description:
+      "Designed and developed a responsive, SEO-optimized website for M Hospitality using Next.js. The site showcases their expertise in hospitality, highlighting their offerings in hotels, resorts, and F&B outlets, while promoting Ras Al Khaimah as a premier destination for leisure and adventure.",
     link: "https://www.m-hospitality.ae",
   },
   {
     img: project6,
     name: "Data Management",
-    description: "",
-    link: "#",
+    description:
+      "Developed a robust React application for The National Health Insurance Company – Daman, the UAE’s leading health insurer. The application features secure login and logout functionality, enabling users to upload multiple file types (PDFs, images, Excel sheets, and CSVs), rename files, download them as a zip, and merge documents into a single PDF. It also includes a PDF compression feature for optimizing large files. Tailored to Daman’s high standards of innovation, the app enhances operational efficiency and aligns with their commitment to providing cutting-edge digital services in the UAE’s health insurance sector.",
+    link: "/",
   },
   {
     img: project7,
     name: "Visitor Management",
-    description: "",
-    link: "#",
+    description:
+      "Developed an ERP-based Visitor Management System to efficiently manage visitor details, featuring an admin panel with role-based access. Employees can create multiple meetings, each with several visitors. The system also integrates an Emirates ID reader for streamlined check-ins, enhancing security and simplifying visitor tracking.",
+    link: "/",
   },
   {
     img: project8,
     name: "Visitor Management Lite",
-    description: "",
-    link: "#",
+    description:
+      "Created a lite version of the Visitor Management System, offering essential features for managing visitor details with a simplified admin panel. This version includes limited role-based access and basic Emirates ID reader functionality.",
+    link: "/",
   },
-  { img: project9, name: "students draw app", description: "", link: "#" },
-  { img: project10, name: "Store Management", description: "", link: "#" },
+  {
+    img: project9,
+    name: "Homey",
+    description:
+      "Developed using MongoDB, Express, React.js, and Node.js, this application allows users to register and book a variety of home services. Users can also become service providers and communicate with providers through a built-in chat feature. The admin has full control to manage and validate all processes, ensuring smooth operation of the platform.",
+    link: "https://homeyapplication.netlify.app/home",
+  },
+  {
+    img: project10,
+    name: "e-commerce website",
+    description:
+      "Developed an eCommerce application with a Node.js and Express backend, MongoDB database, and an EJS-based frontend. Users can register, browse, and purchase items using PayPal or Cash on Delivery (COD) options. The platform includes a coupon system for discounts. Admins have complete control, allowing them to review stock, add products, and validate all processes for smooth operations.",
+    link: "https://healthme.onrender.com/",
+  },
 ];
